036c1618fbd70c9876c5a20540718cc1838b4ca1
Lucas Alves Costa <lucas.alves.costa96@gmail.com>
2020-04-02 22:57:12 -0300

    change Region Context Provider

--- src/context/regionContext.js
+++ src/context/regionContext.js
@@ -3,11 +3,27 @@ import React, { useState } from 'react'
 export const RegionContext = React.createContext()
 
 function RegionContextProvider( { children } ) {
-  const [ region, setRegion ] = useState( 'eu' )
+  const [ region, setRegion ] = useState( 'europe' )
+  const [ countries, setCountries ] = useState()
+
+  async function getCountries( region ) {
+    const data = await fetch( `https://restcountries.eu/rest/v2/${region}` )
+    .then( ( response ) => response.json() )
+    .catch( ( error ) => setError( error ) )
+
+    setCountries( data )
+    setRegion( region )
+    setLoading( false )
+  }
+
   const store = {
-    region: { 
+    countries: {
+      get: countries,
+      set: getCountries
+    },
+    region: {
       get: region,
-      set: setRegion
+      setRegion: setRegion
     }
   }
 
