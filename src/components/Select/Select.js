f87493d521ae0248643bff8031b6b85d33b703bb
Lucas Alves Costa <lucas.alves.costa96@gmail.com>
2020-04-05 22:44:17 -0300

    Update Select Component

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,13 +1,18 @@
 import React from 'react'
 
-function Select( { data, defaultValue, onChange } ) {
-  if ( data === undefined ) {
+function Select( { data, defaultValue, loading, onChange } ) {
+  if ( data === undefined || loading ) {
     return <div>Loading</div>
-  } 
+  }
 
   const list = data.map( item => {
     return (
-      <option key={ item.name }>{item.name}</option>
+      <option
+        key={ item.name }
+        value={ item.alpha2code }
+      >
+        {item.name}
+      </option>
     )
   } )
 
