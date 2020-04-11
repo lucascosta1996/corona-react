8f2eb0fb4e2b7c2f04fc544858552f4eec23197c
Lucas Alves Costa <lucas.alves.costa96@gmail.com>
2020-04-11 01:17:46 -0300

    Detect actual country with ip-json api

--- src/components/layout/SearchContainer.js
+++ src/components/layout/SearchContainer.js
@@ -1,26 +1,49 @@
-import React, { useContext, useEffect, Fragment } from 'react'
+import React, { useContext, useEffect, Fragment, useState } from 'react'
 import Select from '../Select/Select'
 import { RegionContext } from '../../context/regionContext'
 
 function SearchContainer() {
   const context = useContext( RegionContext )
+  const [ firstRender, setFirstRender ] = useState( false )
+  const [ realRegion, setRealRegion ] = useState()
   
-  const regions = [ { name: 'Europe', alpha2code: 'europe' }, { name: 'Asia', alpha2code: 'asia' } ]
+  const regions = [
+    { name: 'Africa', alpha2code: 'Africa' },
+    { name: 'Americas', alpha2code: 'Americas' },
+    { name: 'Asia', alpha2code: 'Asia' },
+    { name: 'Europe', alpha2code: 'Europe' },
+    { name: 'Oceania', alpha2code: 'Oceania' }
+  ]
 
   const onChange = ( event ) => {
     context.region.set( event.target.value )
   }
   
   useEffect( () => {
-    context.region.set( context.region.get )
-  }, [] )
+    if ( !firstRender ) {
+      detectRegion()
+      setFirstRender( true )
+    }
+  }, [ context.region.get ] )
+
+  async function detectRegion() { 
+    const data = await fetch( 'http://ip-api.com/json' ).then( ( response ) => response.json() )
+    setRealRegion( data )
+    setRegion( data.countryCode )
+  }
+
+  async function setRegion( countryCode ) {
+    const data = await fetch( `https://restcountries.eu/rest/v2/alpha/${countryCode}` ).then( ( response ) => response.json() )
+    const region = regions.filter( region => region.name === data.region )
+    context.region.set( data.region )
+  }
 
   return (
     <RegionContext.Consumer>
       { ( context ) => (
         <Fragment>
           <Select data={ context.countriesByRegion.get } loading={ context.loading.get } defaultValue="Select a country" />
-          <Select data={ regions } defaultValue="Select a region" onChange={ onChange } />
+          <Select data={ regions } loading={ context.loading.get } defaultValue={ context.region.get } onChange={ onChange } />
         </Fragment>
       ) }
     </RegionContext.Consumer>
