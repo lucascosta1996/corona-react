ec97c6bc25e7f6298aef64c545f3edfc9d715454
Lucas Alves Costa <lucas.alves.costa96@gmail.com>
2020-04-11 01:20:28 -0300

    Render CountriesList on main component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,13 @@
 import React from "react"
 import RegionContextProvider from "../context/regionContext"
 import SearchContainer from "../components/layout/SearchContainer"
+import CountriesList from "../components/Countries/CountriesList"
 
 export default () => {
-
   return (
     <RegionContextProvider>
       <SearchContainer />
+      <CountriesList />
     </RegionContextProvider>
   )
 }
