c202ec2c0bf570fe1c6382612b46ed9d6a64c5cf
Lucas Alves Costa <lucas.alves.costa96@gmail.com>
2020-04-19 01:12:51 -0300

    Refactor region context

--- src/context/regionContext.js
+++ src/context/regionContext.js
@@ -3,22 +3,28 @@ import React, { useState } from 'react'
 export const RegionContext = React.createContext()
 
 function RegionContextProvider( { children } ) {
-  const [ region, setRegion ] = useState( 'europe' )
-  const [ countries, setCountries ] = useState()
-  const [ loading, setLoading ] = useState( true )
-  const [ error, setError ] = useState()
+  const [ region, setRegion ] = useState( '' );
+  const [ countries, setCountries ] = useState();
+  const [ userCountry, setUserCountry ] = useState();
+  const [ loading, setLoading ] = useState( true );
+  const [ error, setError ] = useState();
 
-  async function getCountries( region ) {
-    setLoading( true )
-
+  async function getCountries( region, countryCode ) {
+    setLoading( true );
     const data = await fetch( `https://restcountries.eu/rest/v2/region/${region}` )
     .then( ( response ) => response.json() )
-    .catch( ( error ) => setError( error ) )
+    .catch( ( error ) => setError( error ) );
 
-    setCountries( data )
-    setRegion( region )
-    setLoading( false )
-  }
+    changeRegion( region );
+    setCountries( data );
+    setLoading( false );
+  };
+
+  function changeRegion( regionInput ) {
+    if ( regionInput !== region ) {
+      setRegion( regionInput );
+    };
+  };
 
   const store = {
     countriesByRegion: {
@@ -28,19 +34,23 @@ function RegionContextProvider( { children } ) {
       get: region,
       set: getCountries
     },
+    userCountry: {
+      get: userCountry,
+      set: setUserCountry
+    },
     loading: {
       get: loading
     },
     error: {
       get: error
     }
-  }
+  };
 
   return (
     <RegionContext.Provider value={ store }>
       { children }
     </RegionContext.Provider>
   )
-}
+};
 
-export default RegionContextProvider
+export default RegionContextProvider;
