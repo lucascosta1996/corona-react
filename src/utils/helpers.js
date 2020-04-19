d50cf8d17067c5b517037d571d9f64e7ba3abb6a
Lucas Alves Costa <lucas.alves.costa96@gmail.com>
2020-04-19 01:13:16 -0300

    Add requests for helpers functions

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,3 +1,13 @@
 export function formatNumberWithComma( number ) {
   return number.toString().replace( /\B(?=(\d{3})+(?!\d))/g, "," );
 };
+
+export async function detectCountry() {
+  const data = await fetch( `https://api.ipgeolocation.io/ipgeo?apiKey=${process.env.REACT_APP_IP_FIND_KEY}` ).then( ( response ) => response.json() );
+  return data;
+};
+
+export async function getCountryInfo( countryCode ) {
+  const country = await fetch( `https://restcountries.eu/rest/v2/alpha/${countryCode}` ).then( ( response ) => response.json() );
+  return country;
+}
