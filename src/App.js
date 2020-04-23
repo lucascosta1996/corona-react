d1114a4fb2690d9bf6d1f4ef57f1048b14f8be48
Lucas Alves Costa <lucas.alves.costa96@gmail.com>
2020-04-23 20:32:12 -0300

    Add lazy load to Countries list component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import './App.scss';
 import RegionContextProvider from './context/RegionContext';
 import FilterContainer from './components/FilterContainer/FilterContainer';
-import CountriesList from './components/Countries/CountriesList';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import CountryPage from './components/CountryPage/CountryPage';
+import Loading from './components/Loading/Loading';
+
+const CountriesListLazy = lazy( () => import( './components/Countries/CountriesList' ) );
 
 function App() {
   return (
     <Router>
-      <RegionContextProvider>
-       <Route exact path="/">
-          <FilterContainer />
-          <CountriesList />
-       </Route>
-       <Route path="/:id" component={ CountryPage } />
-      </RegionContextProvider>
+      <Suspense fallback={ <Loading /> }>
+        <RegionContextProvider>
+        <Route exact path="/">
+            <FilterContainer />
+            <CountriesListLazy />
+        </Route>
+        <Route path="/:id" component={ CountryPage } />
+        </RegionContextProvider>
+      </Suspense>
     </Router>
   );
 };
