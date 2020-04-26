1db74d1a4ba6c523181bc1c8acc1071ee0f40278
Lucas Alves Costa <lucas.alves.costa96@gmail.com>
2020-04-25 23:07:26 -0300

    Add error boundaries

--- src/App.js
+++ src/App.js
@@ -5,21 +5,26 @@ import FilterContainer from './components/FilterContainer/FilterContainer';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import CountryPage from './components/CountryPage/CountryPage';
 import Loading from './components/Loading/Loading';
+import ErrorBoundary from './components/ErrorBoundaries/ErrorBoundaries';
+import Header from './components/Header/Header';
 
 const CountriesListLazy = lazy( () => import( './components/Countries/CountriesList' ) );
 
 function App() {
   return (
     <Router>
-      <Suspense fallback={ <Loading /> }>
-        <RegionContextProvider>
-          <Route exact path="/">
-            <FilterContainer />
-            <CountriesListLazy />
-          </Route>
-          <Route path="/:id" component={ CountryPage } />
-        </RegionContextProvider>
-      </Suspense>
+      <ErrorBoundary>
+        <Suspense fallback={ <Loading /> }>
+          <RegionContextProvider>
+            <Header />
+            <Route exact path="/">
+              <FilterContainer />
+              <CountriesListLazy />
+            </Route>
+            <Route path="/:id" component={ CountryPage } />
+          </RegionContextProvider>
+        </Suspense>
+      </ErrorBoundary>
     </Router>
   );
 };
