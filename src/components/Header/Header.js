d9b0b231939a826b3b9b7b6a1f4fbbe5453a38e4
Lucas Alves Costa <lucas.alves.costa96@gmail.com>
2020-04-25 23:08:31 -0300

    Create Header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './assets/styleHeader.scss';
+import { Link } from 'react-router-dom';
+
+function Header() {
+  return (
+    <header className="header">
+      <Link to="/">
+        <h1>Coronavirus (COVID-19)</h1>
+      </Link>
+    </header>
+  );
+}
+
+export default React.memo( Header );
